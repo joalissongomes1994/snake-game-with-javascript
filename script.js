56a95f1aa7ecf0b45e0b4d140ce00d6e111fad7a
Joalisson Gomes <joalissongomes1994@gmail.com>
2021-01-29 03:34:18 -0300

    Create background

---
+++ script.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById('snake');
+var context = canvas.getContext('2d');
+let box = 32;
+
+function criarBG() {
+  context.fillStyle = "lightgreen";
+  context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+criarBG();
