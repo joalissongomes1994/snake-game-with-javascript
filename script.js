9d5f384ba824ceb85da98fdb27c7303d8b8a4fe1
Joalisson Gomes <joalissongomes1994@gmail.com>
2021-01-29 23:00:06 -0300

    Define game over

--- script.js
+++ script.js
@@ -46,6 +46,13 @@ function startGame() {
   if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
 
+  for(i = 1; i < snake.length; i++) {
+    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      setInterval(startGame);
+      alert("GAME OVER :(");
+    }
+  }
+
   createBG();
   createSnake();
   drawFood();
@@ -64,7 +71,6 @@ function startGame() {
   else {
     food.x= Math.floor( Math.random() * 15 + 1) * box; 
     food.y= Math.floor( Math.random() * 15 + 1) * box 
-
   }
 
   let newSnakeHead = {
