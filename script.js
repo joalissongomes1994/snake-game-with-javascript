9a3dd0842d4c70f652a7e32c12037705a803c52c
Joalisson Gomes <joalissongomes1994@gmail.com>
2021-01-29 23:50:17 -0300

    Add styling to the body

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function startGame() {
   for(i = 1; i < snake.length; i++) {
     if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
       setInterval(startGame);
-      alert("GAME OVER :(");
+      alert("GAME OVER: To restart reload the page!");
     }
   }
 
